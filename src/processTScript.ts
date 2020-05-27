a729325014ff80813f6d16bbb83fe31a6d18f0e4
Robert Geil <robert@rgeil.net>
2020-05-27 11:43:32 -0400

    Refined processing typescript, including full build of express server, adding tsconfig.json, and bug fixes

--- src/processTScript.ts
+++ src/processTScript.ts
@@ -1,11 +1,19 @@
 import ts from "typescript";
+import { js as beautify } from "js-beautify";
+import assert from "assert";
+
 import { functions, packages, getHeader } from "./shared";
 
-function processTScript(raw: string, functions: functions) {
+/** buildProgram
+ *
+ * @param {string} raw the raw program to be converted into an API
+ * @return {ts.Program} an in-memory, type-checked TypeScript program
+ */
+function buildProgram(raw: string): ts.Program {
   const options = ts.getDefaultCompilerOptions();
   const realHost = ts.createCompilerHost(options, true);
 
-  const sourcePath = "/in-memory-file.ts";
+  const sourcePath = "/index.ts";
   const sourceFile = ts.createSourceFile(
     sourcePath,
     raw,
@@ -61,8 +69,26 @@ function processTScript(raw: string, functions: functions) {
         diagnostics.map((m) => m.messageText).join("\n")
     );
   }
+  return program;
+}
 
+/**
+ * extractPublicFunctions
+ *
+ * Checks to ensure that the provided script raw is valid Typescript, and
+ * strips out all functions from the top level, returning those which are
+ * marked in @param functions
+ *
+ * @throws if functions requested in functions are missing from the raw script
+ *
+ * @param {ts.Program} program
+ * @param {functions} functions
+ * @return {ts.FunctionDeclaration[]}
+ */
+function extractPublicFunctions(program: ts.Program, functions: functions) {
   const parsedFunctions: string[] = [];
+  const sourceFile = program.getSourceFile("/index.ts");
+  assert(sourceFile);
   sourceFile.forEachChild((c) => {
     if (ts.isFunctionDeclaration(c)) {
       parsedFunctions.push(c.name!!.text);
@@ -86,27 +112,75 @@ Missing (${missingFunctions.join(",")})`
   return functionsToBeAPId;
 }
 
-function generatePackageJSON(packages: packages) {
+function generatePackageJSON(packages: packages, devPackages: packages) {
   return JSON.stringify(
     {
       name: "generator-js",
       version: "1.0.0",
       description: "",
-      main: "index.ts",
+      main: "dist/index.js",
       scripts: {
+        prepare: "tsc",
         test: 'echo "Error: no test specified" && exit 1',
       },
       author: "",
       license: "ISC",
       dependencies: packages,
+      devDependencies: devPackages,
     },
     null,
     2
   );
 }
 
-function convertFuncToRoute(func: ts.FunctionDeclaration): string {
-  return "app.get()\n";
+function generateTsconfig() {
+  return JSON.stringify(
+    {
+      compilerOptions: {
+        lib: [
+          "ES2016",
+          "DOM",
+        ] /* Specify library files to be included in the compilation. */,
+        declaration: true /* Generates corresponding '.d.ts' file. */,
+        sourceMap: true /* Generates corresponding '.map' file. */,
+        outDir: "./dist" /* Redirect output structure to the directory. */,
+        strict: true /* Enable all strict type-checking options. */,
+        moduleResolution: "node",
+        esModuleInterop: true,
+        skipLibCheck: true,
+        forceConsistentCasingInFileNames: true,
+      },
+      // include: ["./src/**/*", "./index.ts"],
+      // exclude: ["./tests/**/*"],
+    },
+    null,
+    2
+  );
+}
+
+function convertFuncToRoute(
+  func: ts.FunctionDeclaration,
+  checker: ts.TypeChecker
+): string {
+  let rt = checker.typeToString(checker.getTypeAtLocation(func));
+  rt = rt.substr(rt.indexOf("=>") + 3);
+  if (func.parameters.length === 0) {
+    console.log(rt);
+    // Perform a "GET" request
+    return `app.get('/${func.name}', (req, res) => {
+      const response = ${func.name}();
+      res.send(JSON.stringify(response));
+      })`;
+  } else {
+    const requestParams = func.parameters
+      .map((f) => `body.${f.name.getText()}`)
+      .join(", ");
+    return `app.post('/${func.name?.getText()}', (req, res) => {
+      const body = req.body;
+      const response = ${func.name?.getText()}(${requestParams});
+      res.send(JSON.stringify(response));
+    });`;
+  }
 }
 
 export default function buildTSExpress(
@@ -114,28 +188,48 @@ export default function buildTSExpress(
   functions: functions,
   packages: packages
 ) {
-  const processed = processTScript(raw, functions);
+  const program = buildProgram(raw);
+  const typeChecker = program.getTypeChecker();
+  const processed = extractPublicFunctions(program, functions);
   // Check if all items can be represented as a GET request (ie no args)
   const allGet = processed.every((p) => p.parameters.length === 0);
   if (!allGet) {
     packages["body-parser"] = "latest";
   }
-  const packageJSON = generatePackageJSON({
-    ...packages,
-    express: "~4",
-    "is-promise": "^4",
-  });
+  const packageJSON = generatePackageJSON(
+    {
+      ...packages,
+      express: "~4",
+      "@types/express": "~4",
+      "is-promise": "^4",
+    },
+    { typescript: "latest" }
+  );
   let response = getHeader();
-  response += 'import express from "express";';
+  response += "// Setup server to send API routes\n";
+  response += 'import express from "express";\n';
+  response += "const app = express();\n";
+  if (!allGet) {
+    response += `// Add body parser for receiving POST requests
+      const bodyParser = require('body-parser');
+      app.use(bodyParser.json());\n`;
+  }
   // Sort all exported functions to allow for correct insertion of functions
   processed.sort((e1, e2) => e2.pos - e1.pos);
   let prevPos = 0;
   processed.forEach((func) => {
     response += "\n" + raw.substring(prevPos, func.end) + "\n";
-    const newRoute = convertFuncToRoute(func);
+    const newRoute = convertFuncToRoute(func, typeChecker);
     prevPos = func.end;
     response += newRoute;
   });
   response += raw.substring(prevPos);
-  return { index: response, packageJSON };
+  // Add action to listen on local host
+  response +=
+    "app.listen(3000, () => console.log('API listening at http://localhost:3000'));";
+  return {
+    index: beautify(response),
+    packageJSON,
+    tsConfig: generateTsconfig(),
+  };
 }
